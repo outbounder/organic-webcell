33b8c4539c29fdb9d2ee64330334761983134b3b
Boris Filipov <obiwonn@gmail.com>
2013-04-29 16:16:59 +0300

    improved documentation style and fixed typos

--- membrane/ExpressHttpServer.js
+++ membrane/ExpressHttpServer.js
@@ -13,41 +13,48 @@ It is general purpose membrane hole for handling any incoming http requests.
 
 organel | dna & defaults:
 
-* middlware - [ `Middleware Object` ]
+* middleware - [ `Middleware Object` ]
+
+  #### Middleware Object 
+  
+  * `String` - full path to Middlware source code or
+
+            {
+             source: full path to Middleware source code
+             ... `config` of Middlware
+            }
+
+  #### middleware source code example
+
+        module.exports = function(`config`, httpServer){
+         var app = httpServer.app; // express app object
+         return function(req, res, next) {} // optional, will be passed to app.use(fn)
+        }
+
 * afterware - [ `Middleware Object` ]
+
+  see `middleware` attribute for definition of `Middleware Object`
+
 * routes - { `path: Route object` }
+
+  ### path: `Route object`
+
+  * `path` is String passed to `app.all(path, fn)`
+  * `Route object` is Chemical to be emitted in plasma including:
+    * `req` express object
+    * `res` express object
+  
+    expected callback chemical will trigger `res.send` based on returned:
+      * `content-type` - String
+      * `data` - mixed
+      * `statusCode` - Number, defaults to `200`
+
 * notfoundRoute - `Route object`
+
+  see `routes` attribute for Route object details. This simply adds middleware to express server emitting chemical with given route object definition.
+
 * log - `false`
 * port - `1337`
-
-
-### Midleware Object 
- * `String` - full path to Middlware source code or
-    
-    {
-       source: full path to Middleware source code
-       ... <`config` of Middlware>
-    }
-
-#### middleware source code example
-
-    module.exports = function(`config`, httpServer){
-      var app = httpServer.app; // express app object
-      return function(req, res, next) {} // optional, will be passed to app.use(fn)
-    }
-
-### path: `Route object`
-
-* `path` is String passed to `app.all(path, fn)`
-* `Route object` is Chemical to be emitted in plasma including:
-  * `req` express object
-  * `res` express object
-  
-  expected callback chemical will trigger `res.send` based on returned:
-    * `content-type` - String
-    * `data` - mixed
-    * `statusCode` - Number, defaults to `200`
-
 
 
 
