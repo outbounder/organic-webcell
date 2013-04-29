648c7ee6c2a3bd726345427979e9a44441a8f3e7
Boris Filipov <obiwonn@gmail.com>
2013-04-29 23:08:36 +0300

    added minimal documentation

--- plasma/ExpressHttpActions.js
+++ plasma/ExpressHttpActions.js
@@ -8,12 +8,44 @@ var Actions = require("../lib/Actions");
 var Context = require("../lib/Context");
 var DirectoryTree = require("../lib/DirectoryTree");
 
+/* organel | 
+
+The Organelle is responsible for mounting Http Routes to the express app wrapped by ExpressHttpServer organelle or any other implementation providing the same interface.
+
+It is roughly described in [wisdom about ExpressHttpActions](http://wisdom.camplight.net/wisdom/5110e488c489a7ef6d000043/Organic---ExpressHttpActions) 
+
+* `mount`: String
+* `cwd`: Object
+* `actions`: String
+
+  provided full path to directory containing `actions` defined within javascript files with the following source code definition:
+
+        module.exports = function(config) {
+          return {
+            "*": function(req, res, next) {},
+            "GET": function(req, res, next) {},
+            "POST": function(req, res, next) {}, 
+            "PUT": function(req, res, next) {}, 
+            "DELETE": function(req, res, next) {}, 
+            "OPTIONS": function(req, res, next) {}
+          }
+        }
+
+* `actionHelpers`: String
+
+  provided full path to directory containing `actionHelpers` defined within commonjs modules. Any exports from them will be attached to context of the `actions` within object with the same name as the corresponding action helper.
+
+* `log`: false
+
+  will print any found action routes to the stdout
+
+*/
+
 /* incoming | HttpServer
 
 * data - ExpressHttpServer instance
 
 */
-
 
 module.exports = function ExpressHttpActions(plasma, config){
   Organel.call(this, plasma);
