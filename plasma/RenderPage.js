3c2122f0e7ec30b7b8a6a60966600da5b5c0fcbc
Boris Filipov <obiwonn@gmail.com>
2013-04-30 02:43:08 +0300

    Added docs to RenderPage

--- plasma/RenderPage.js
+++ plasma/RenderPage.js
@@ -5,9 +5,37 @@ var jade = require("jade");
 var fs = require("fs");
 var _ = require("underscore");
 
+/* organel | 
+
+Organelle responsible for rendering given page template as html. It wraps `jade` template engine to do so.
+
+
+* `cwd` - Object
+
+  *optional* object containing key:values, where values will be prefixed with `process.cwd()` and placed within config itself with coresponding keys
+
+  Useful to provide `root` value relative to current working directory
+
+* `root` - String
+
+  *optional*, represents the root value to be added in forming page template path.
+
+* `useCache` - false
+
+  Will instruct compiling the template to memory and further use it without any disk IO.
+
+* `page` - String
+
+  *optional*, represents the default page template path to be used if missing in incoming chemicals*/
+
 /* incoming | RenderPage
 
-* page - String
+When receiving such chemical the organelle will use it to render the template specified as path within the `page` attribute.  The chemical will be passed as data to the template, so any of its properties will be directly accessible in the template.
+
+* `page` - String
+* `root` - String
+
+  *optional*
 
 */
 
